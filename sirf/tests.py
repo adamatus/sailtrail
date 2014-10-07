de055f0c5ce5136fbea1fa9a39fd9b02727fe5ef
Adam Riggall <adam@riggall.info>
2014-10-07 10:29:11 -0700

    Add SBN (SiRF) parsing, with basic tests
    - Tests only use single SBN file, test for valid output
    - Not currently testing other SBN files (mostly just Data)

---
+++ sirf/tests.py
@@ -0,0 +1,26 @@
+import unittest
+import os
+import sirf
+
+ASSET_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                         'test_assets')
+TEST_FILE = os.path.join(ASSET_DIR, 'test.sbn')
+
+
+class SiRFTest(unittest.TestCase):
+    """Tests for SiRF Processing"""
+
+    def test_reading_of_sirf_file(self):
+        p = sirf.read_sbn(TEST_FILE)        
+        self.assertEquals(679, p.rx_cnt)
+        self.assertEquals('2013/7/9', p.pktq[1]['date'])
+        self.assertEquals('23:54:47', p.pktq[1]['time'])
+        self.assertEquals('4+-SV KF', p.pktq[1]['fixtype'])
+        self.assertAlmostEquals(43.0771931, p.pktq[1]['latitude'])
+        self.assertAlmostEquals(-89.4007119, p.pktq[1]['longitude'])
+        self.assertEquals('2013/7/10', p.pktq[620]['date'])
+        self.assertEquals('0:5:0', p.pktq[620]['time'])
+        self.assertEquals('4+-SV KF', p.pktq[620]['fixtype'])
+        self.assertAlmostEquals(43.0771587, p.pktq[620]['latitude'])
+        self.assertAlmostEquals(-89.4006786, p.pktq[620]['longitude'])
+
