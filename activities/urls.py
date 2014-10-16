925687127176a199f165da49424fac992a5967ad
Adam Riggall <adam@riggall.info>
2014-10-15 22:51:33 -0700

    Implement trim start/end of track/speed view

--- activities/urls.py
+++ activities/urls.py
@@ -6,6 +6,8 @@ urlpatterns = patterns(
     '',
     url(r'^upload$', 'activities.views.upload', name='upload'),
     url(r'(\d+)/delete$', 'activities.views.delete', name='delete_activity'),
+    url(r'(\d+)/trim$', 'activities.views.trim', name='trim_activity'),
+    url(r'(\d+)/untrim$', 'activities.views.untrim', name='untrim_activity'),
     url(r'(\d+)/details/$', 'activities.views.details', name='details'),
     url(r'(\d+)/$', 'activities.views.view', name='view_activity'),
     url(r'^jasmine/track_viewer$', 
