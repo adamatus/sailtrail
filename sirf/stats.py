d0ccbed02531a38fe27c0cf632f3ab4587b8bc62
Adam Riggall <adam@riggall.info>
2014-10-22 21:35:11 -0700

    Add basic distance calc to sirf stats
    - Currently using Stats (which reads file)
    - Currently very slow, just looping through adjacent points,
      computing the distance, then summing them, all in basic python

--- sirf/stats.py
+++ sirf/stats.py
@@ -5,6 +5,8 @@ import pytz
 
 from pint import UnitRegistry
 
+from math import sin, cos, sqrt, acos, atan2, radians
+
 
 class Stats(object):
 
@@ -58,6 +60,13 @@ class Stats(object):
     def max_speed(self):
         return max(self.speeds)
 
+    def distance(self, method='EquirecApprox'):
+        distance = 0
+        for i in range(len(self.tracks)-1):
+            distance += self._distance(self.tracks[i], 
+                                       self.tracks[i+1], method)
+        return distance * (self.units.m * 1000)
+
     def _filter_timepoints(self):
         self.timepoints = [x for x in self.timepoints 
                            if x is not None and x['satlst'] >= 3]
@@ -66,3 +75,33 @@ class Stats(object):
         return datetime.strptime('{} {}'.format(time, date),
                                  '%H:%M:%S %Y/%m/%d').replace(tzinfo=pytz.UTC)
                         
+    def _distance(self, pos1, pos2, method='Haversine'):
+        # Calculation formats from here 
+        # http://www.movable-type.co.uk/scripts/latlong.html
+
+        R = 6371.0  # Earth's radius in km
+        lat1 = radians(pos1['lat'])
+        lat2 = radians(pos2['lat'])
+        lon1 = radians(pos1['lon'])
+        lon2 = radians(pos2['lon'])
+
+        # Haversine
+        if method == 'Haversine':
+            dlon = lon2 - lon1
+            dlat = lat2 - lat1
+
+            a = (sin(dlat/2))**2 + cos(lat1) * cos(lat2) * (sin(dlon/2))**2
+            c = 2 * atan2(sqrt(a), sqrt(1 - a))
+            return R * c
+
+        elif method == 'SphLawCos':
+            dlon = lon2 - lon1
+
+            return acos(sin(lat1)*sin(lat2) + 
+                        cos(lat1)*cos(lat2)*cos(dlon)) * R
+
+        else:  # method == Equirectangular approx
+            x = (lon2-lon1) * cos((lat1+lat2)/2)
+            y = (lat2-lat1)
+            return sqrt(x**2 + y**2) * R
+
--- sirf/tests.py
+++ sirf/tests.py
@@ -7,14 +7,14 @@ from datetime import date, time, timedelta
 
 ASSET_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                          'test_assets')
-TEST_FILE = os.path.join(ASSET_DIR, 'test.sbn')
+TEST_FILE = os.path.join(ASSET_DIR, 'test-small.sbn')
 
 
 class SiRFTest(unittest.TestCase):
     """Tests for SiRF Processing"""
 
     def test_reading_of_sirf_file(self):
-        p = sirf.read_sbn(TEST_FILE)        
+        p = sirf.read_sbn(os.path.join(ASSET_DIR, 'test.sbn'))        
         self.assertEquals(679, p.rx_cnt)
         self.assertEquals('2013/07/09', p.pktq[1]['date'])
         self.assertEquals('23:54:47', p.pktq[1]['time'])
@@ -34,29 +34,36 @@ class StatsTest(unittest.TestCase):
         self.stats = Stats(TEST_FILE)
 
     def test_get_start_time(self):
-        self.assertEquals(time(23, 54, 47), self.stats.start_time)
+        self.assertEquals(time(2, 45, 13), self.stats.start_time)
 
     def test_get_end_time(self):
-        self.assertEquals(time(0, 5, 58), self.stats.end_time)
+        self.assertEquals(time(2, 45, 40), self.stats.end_time)
 
     def test_get_start_date(self):
-        self.assertEquals(date(2013, 7, 9), self.stats.start_date)
+        self.assertEquals(date(2014, 9, 28), self.stats.start_date)
 
     def test_get_duration(self):
-        self.assertEquals(timedelta(0, 671), self.stats.duration)
+        self.assertEquals(timedelta(0, 27), self.stats.duration)
 
     def test_get_tracks(self):
         tracks = self.stats.tracks
-        self.assertEquals(672, len(tracks))
-        self.assertAlmostEquals(-89.4007119, tracks[0]['lon'])
-        self.assertAlmostEquals(43.0771931, tracks[0]['lat'])
+        self.assertEquals(28, len(tracks))
+        self.assertAlmostEquals(-122.6243636, tracks[0]['lon'])
+        self.assertAlmostEquals(45.604354, tracks[0]['lat'])
 
     def test_get_speeds(self):
         speeds = self.stats.speeds
-        self.assertEquals(672, len(speeds))
-        self.assertAlmostEquals(.11, speeds[0].magnitude)
+        self.assertEquals(28, len(speeds))
+        self.assertAlmostEquals(2.19, speeds[0].magnitude)
 
     def test_get_max_speed(self):
-        self.assertEqual('1.05 m / s', '{:~}'.format(self.stats.max_speed))
+        self.assertEqual('2.53 m / s', '{:~}'.format(self.stats.max_speed))
 
+    def test_get_distance(self):
+        self.assertAlmostEquals(
+            59.12590197, self.stats.distance(method='Haversine').magnitude)
+        self.assertAlmostEquals(
+            59.11451088, self.stats.distance(method='SphLawCos').magnitude)
+        self.assertAlmostEquals(
+            59.12590197, self.stats.distance(method='Equirect').magnitude)
 
