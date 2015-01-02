1af27a6d14c3751a0a928d96706e43a9cabc39b2
Adam Riggall <adam@riggall.info>
2015-01-02 13:03:36 -0800

    Remove old django/jasmine JS tests

--- activities/tests/test_js.py
+++
@@ -1,9 +0,0 @@
-from djangojs.runners import JsTemplateTestCase, JasmineSuite
-
-
-class TrackViewerJSTests(JasmineSuite, JsTemplateTestCase):
-    template_name = 'tests/track_viewer.html'
-
-
-class SpeedViewerJSTests(JasmineSuite, JsTemplateTestCase):
-    template_name = 'tests/speed_viewer.html'
