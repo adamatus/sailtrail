af5928922b6fa103c228b1773600bf153d65e7f4
Adam Riggall <adam@riggall.info>
2015-01-01 18:52:45 -0800

    Add sass via grunt (with watch), livereload JS + SASS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,23 @@
 module.exports = function(grunt) {
 
-	var jslist = ['activities/static/activities/js/*.js'];
+	var jslist = ['activities/static/activities/js/*.js'],
+			jsbundlelist = ['activities/static/activities/js/*.bundle.js'],
+			sasslist = ['activities/static/activities/css/scss/*.scss'];
+			csslist = ['activities/static/activities/css/*.css'];
 
   // Project configuration
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
 		watch: {
-		
+			sass: {
+				files: sasslist,
+				tasks: ['sass'],
+			},
+			livereload: {
+				options: { livereload: true },
+				files: csslist.concat(jsbundlelist),
+			},
 		},
 
 		browserify: {
@@ -20,11 +30,20 @@ module.exports = function(grunt) {
 					watch: true
 				}
 			}
+		},
+
+		sass: {
+			dev: {
+				files: {
+					"activities/static/activities/css/plots.css": "activities/static/activities/css/scss/plots.scss"
+				}
+			}
 		}
   });
 
   // Load plugins here
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Register tasks here
