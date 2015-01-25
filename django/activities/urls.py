6c54a8298e90c3aa8baea39179fac54afe47a642
Adam Riggall <adam@riggall.info>
2015-01-24 20:51:06 -0800

    Reorder activities URLs

--- django/activities/urls.py
+++ django/activities/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     '',
     url(r'^upload$', 'activities.views.upload', name='upload'),
-    url(r'(\d+)/delete$', 'activities.views.delete', name='delete_activity'),
+
     url(r'(\d+)/tracks/(\d+)/delete$',
         'activities.views.delete_track',
         name='delete_track'),
@@ -13,12 +13,14 @@ urlpatterns = patterns(
     url(r'(\d+)/tracks/(\d+)/untrim$',
         'activities.views.untrim',
         name='untrim_track'),
-    url(r'(\d+)/details/$', 'activities.views.details', name='details'),
     url(r'(\d+)/tracks/(\d+)/$',
         'activities.views.view_track',
         name="view_track"),
     url(r'(\d+)/tracks/upload$',
         'activities.views.upload_track',
         name='upload_track'),
+
+    url(r'(\d+)/details/$', 'activities.views.details', name='details'),
+    url(r'(\d+)/delete$', 'activities.views.delete', name='delete_activity'),
     url(r'(\d+)/$', 'activities.views.view', name='view_activity'),
 )
