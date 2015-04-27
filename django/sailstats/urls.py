867d9fbcaf3a384b1439476a8b954b48923320d9
Adam Riggall <adam@riggall.info>
2015-04-26 17:40:09 -0700

    Update URLs to new 1.8 style

--- django/sailstats/urls.py
+++ django/sailstats/urls.py
@@ -1,17 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns(
-    '',
-    url(r'^$', 'activities.views.home_page', name='home'),
+from activities import views as activity_views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    url(r'^$', activity_views.home_page, name='home'),
     url(r'^activities/', include('activities.urls')),
+    url('^register/$', activity_views.register, name='register'),
+
     url(r'^login/$',
-        'django.contrib.auth.views.login',
+        auth_views.login,
         {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$',
-        'django.contrib.auth.views.logout',
+        auth_views.logout,
         {'next_page': '/'}, name='logout'),
-    url('^register/$', 'activities.views.register', name='register'),
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^djangojs/', include('djangojs.urls')),
-)
+]
