c5a6f636d951770f403500b9ca2886b34f8c4576
Adam Riggall <adam@riggall.info>
2015-05-01 18:18:13 -0700

    Load tracks directly into DB
    * We've not stopped storing the file on the server, instead loading it
    straight into the database.  This will eventually (hopefully) speed up
    the unit tests, as they won't have to do any disk IO

--- django/sailstats/settings/common.py
+++ django/sailstats/settings/common.py
@@ -98,3 +98,19 @@ STATICFILES_FINDERS = (
 STATIC_ROOT = os.path.join(BASE_DIR, '../../../static')
 
 BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, '../../')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
