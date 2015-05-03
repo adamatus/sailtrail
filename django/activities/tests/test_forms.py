22d4582c1885c43644f6ce771baff61f3e215c34
Adam Riggall <adam@riggall.info>
2015-05-02 22:07:03 -0700

    More py.test refactoring (units)

--- django/activities/tests/test_forms.py
+++ django/activities/tests/test_forms.py
@@ -1,52 +1,31 @@
-from django.test import TestCase
-
-import shutil
-import tempfile
-import os.path
-
+import pytest
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 from activities.forms import UploadFileForm, ActivityDetailsForm
 from activities.models import ActivityDetail
-
 from .factories import ActivityFactory
 
-ASSET_PATH = os.path.join(os.path.dirname(__file__),
-                          'assets')
 
-
-class UploadfileFormTest(TestCase):
-
-    def setUp(self):
-        self.temp_dir = tempfile.mkdtemp()
-
-    def tearDown(self):
-        shutil.rmtree(self.temp_dir)
+class TestUploadFileForm:
 
     def test_form_renders_file_upload_field(self):
-        """should include the correct input field id"""
         form = UploadFileForm()
-        self.assertIn('id_upfile', form.as_p())
+        assert 'id_upfile' in form.as_p()
 
     def test_form_save(self):
-        """[save] should be valid with good input"""
-        with self.settings(MEDIA_ROOT=self.temp_dir):
-            with open(os.path.join(ASSET_PATH, 'tiny.SBN'), 'rb') as f:
-                sbn_bin = f.read()
-            upfile = SimpleUploadedFile('test.txt', sbn_bin)
-            form = UploadFileForm({}, {'upfile': upfile})
-            self.assertTrue(form.is_valid())
+        upfile = SimpleUploadedFile('test.txt', b'test')
+        form = UploadFileForm({}, {'upfile': upfile})
+        assert form.is_valid() is True
 
 
-class ActivitydetailsFormTest(TestCase):
+@pytest.mark.django_db
+class TestActivityDetailsForm:
 
     def test_form_renders_correct_fields(self):
-        """should include the correct input field id"""
         form = ActivityDetailsForm()
-        self.assertIn('id_name', form.as_p())
+        assert 'id_name' in form.as_p()
 
     def test_from_save(self):
-        """[save] should succeed with valid name"""
         a = ActivityFactory.create()
         form = ActivityDetailsForm({'name': 'Test',
                                     'description': '',
@@ -54,6 +33,5 @@ class ActivitydetailsFormTest(TestCase):
                                     'activity_id': a.id})
         form.is_valid()
         upactivity = form.save()
-        self.assertNotEquals(upactivity, None)
-        self.assertEqual(upactivity,
-                         ActivityDetail.objects.first())
+        assert upactivity is not None
+        assert upactivity == ActivityDetail.objects.first()
