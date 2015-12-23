dc0f7f7baf8de182805469c5603598826a42ce8d
Adam Riggall <adam@riggall.info>
2015-12-22 21:20:57 -0800

    Update user page activity list to use pagination

--- django/users/tests/test_views.py
+++ django/users/tests/test_views.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch, sentinel
+from unittest.mock import patch, sentinel, Mock
 
 from django.test import RequestFactory
 
@@ -16,25 +16,35 @@ class TestUserView(unittest.TestCase):
         self.request.user = self.user
         view = UserView()
         view.request = self.request
-        view.object = self.user
+        view.object_list = [1, 2, 3]
+        view.paginate_by = 1
+        view.page_kwarg = "1"
+        view.kwargs = {"1": 1}
         self.view = view
 
     @patch('users.views.Helper')
     def test_get_context_data_populates_activities(self, mock_helper):
 
-        mock_helper.get_users_activities.return_value = \
-            sentinel.activity_query_set
         mock_helper.summarize_by_category.return_value = sentinel.summary
+
+        self.view.get_queryset = Mock()
+        self.view.get_queryset.return_value = sentinel.activity_query_set
 
         context = self.view.get_context_data()
 
-        mock_helper.get_users_activities.assert_called_once_with(self.user,
-                                                                 self.user)
         mock_helper.summarize_by_category.assert_called_once_with(
             sentinel.activity_query_set)
 
-        self.assertEqual(context['activities'], sentinel.activity_query_set)
         self.assertEqual(context['summaries'], sentinel.summary)
+
+    @patch('users.views.Helper')
+    def test_get_context_data_includes_user(self, mock_helper):
+
+        self.view.user = sentinel.user
+
+        context = self.view.get_context_data()
+
+        self.assertEqual(context['view_user'], sentinel.user)
 
     def test_includes_upload_form_mixin(self):
         self.assertIsInstance(self.view, UploadFormMixin)
