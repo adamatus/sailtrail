fcf6b2d639d27eabcce611b0abec8da22f467f72
Adam Riggall <adam@riggall.info>
2015-12-31 13:59:57 -0800

    Fix download link

---
+++ django/api/migrations/0003_make_activitytrackfile_onetoone.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0002_add_activitytrackfile_model'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='activitytrackfile',
+            name='track',
+            field=models.OneToOneField(to='api.ActivityTrack', related_name='original_file'),
+        ),
+    ]
