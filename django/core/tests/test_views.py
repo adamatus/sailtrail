f9079f2d3e23ec1adea2c5a6d48f4f25824b9ca2
Adam Riggall <Adam.Riggall@nike.com>
2016-01-04 14:56:34 -0800

    Add coverage for core.views

--- django/core/tests/test_views.py
+++ django/core/tests/test_views.py
@@ -1,9 +1,10 @@
 import unittest
 
+from django.test import RequestFactory
 from django.views.generic import TemplateView
 
 from core.forms import UploadFileForm
-from core.views import UploadFormMixin
+from core.views import UploadFormMixin, ErrorTemplateView
 
 
 class TestUploadFormMixing(unittest.TestCase):
@@ -17,3 +18,20 @@ class TestUploadFormMixing(unittest.TestCase):
         context = view.get_context_data()
 
         self.assertIsInstance(context['upload_form'], UploadFileForm)
+
+
+class TestErrorTemplateView:
+
+    def test_as_error_view_includes_status_code(self):
+
+        class TestErrorView(ErrorTemplateView):
+            status_code = 113
+            template_name = '400.html'
+
+        view = TestErrorView.as_error_view()
+
+        request = RequestFactory(method="GET")
+        request.method = 'GET'
+        response = view(request)
+
+        assert response.status_code == 113
