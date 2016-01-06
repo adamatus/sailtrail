11e7a070b6508696d4e99f64636b3152f5afa3b0
Adam Riggall <Adam.Riggall@nike.com>
2016-01-06 08:40:29 -0800

    Cleanup more activities tests

--- django/activities/tests/test_integration.py
+++ django/activities/tests/test_integration.py
@@ -22,6 +22,26 @@ with open(os.path.join(ASSET_PATH, 'tiny.SBN'), 'rb') as f:
     SBN_BIN = f.read()
 
 
+@pytest.mark.django_db
+@pytest.mark.integration
+class TestActivityDetailsFormIntegration:
+
+    def test_form_renders_correct_fields(self):
+        form = ActivityDetailsForm()
+        assert 'id_name' in form.as_p()
+
+    def test_form_save(self):
+        a = ActivityFactory.create()
+        form = ActivityDetailsForm({'name': 'Test',
+                                    'description': '',
+                                    'category': 'IB'},
+                                   instance=a)
+        form.is_valid()
+        upactivity = form.save()
+        assert upactivity is not None
+        assert upactivity.name == 'Test'
+
+
 @pytest.mark.integration
 class TestFileUploadViewIntegration(TestCase):
 
--- django/activities/tests/test_sitemap.py
+++ django/activities/tests/test_sitemap.py
@@ -1,7 +1,8 @@
 import unittest
-from unittest.mock import patch, MagicMock, sentinel
+from unittest.mock import patch, MagicMock, sentinel, Mock
 
 from activities.sitemap import ActivitySitemap
+from api.models import Activity
 
 
 class TestSitemap(unittest.TestCase):
@@ -11,17 +12,21 @@ class TestSitemap(unittest.TestCase):
 
     @patch('activities.sitemap.get_public_activities')
     def test_items_returns_result_of_helper(self, helper_mock: MagicMock):
+        # Given a get_activities mock that returns a sentinel
         helper_mock.return_value = sentinel.activities
 
+        # When getting the sitemap items
         items = self.sitemap.items()
 
-        helper_mock.assert_called_with()
+        # Then the sentinel will be returned
         assert items == sentinel.activities
 
     def test_lastmod_returns_activity_modified_date(self):
-        activity = MagicMock()
-        activity.modified = sentinel.date
+        # Given a mock activity with a sentinel date
+        activity = Mock(spec=Activity, modified=sentinel.date)
 
+        # When getting the last modified date for the item
         lastmod = self.sitemap.lastmod(activity)
 
+        # Then the sentinel is returned
         assert lastmod == sentinel.date
