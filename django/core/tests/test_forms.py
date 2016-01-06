765d0884406eab496b1f328ebbf51b7c3099267c
Adam Riggall <Adam.Riggall@nike.com>
2016-01-06 08:54:16 -0800

    Cleanup core tests

--- django/core/tests/test_forms.py
+++ django/core/tests/test_forms.py
@@ -6,10 +6,21 @@ from core.forms import UploadFileForm
 class TestUploadFileForm:
 
     def test_form_renders_file_upload_field(self):
+        # Given an upload file form
         form = UploadFileForm()
-        assert 'id_upfile' in form.as_p()
+
+        # When the form is rendered
+        rendered_form = form.as_p()
+
+        # Then the upfile field is included
+        assert 'id_upfile' in rendered_form
 
     def test_form_save(self):
+        # Given a fake uploaded file
         upfile = SimpleUploadedFile('test.txt', b'test')
+
+        # When using the file in a new form
         form = UploadFileForm({}, {'upfile': upfile})
+
+        # Then the form is valid
         assert form.is_valid() is True
--- django/core/tests/test_views.py
+++ django/core/tests/test_views.py
@@ -14,9 +14,13 @@ class TestUploadFormMixing(unittest.TestCase):
         template_name = 'dummy_template.html'
 
     def test_context_data_adds_form(self):
+        # Given a dummy view that uses the upload form mixin
         view = self.DummyView()
+
+        # When getting context data
         context = view.get_context_data()
 
+        # Then the upload_form is included in the context
         assert isinstance(context['upload_form'], UploadFileForm)
 
 
@@ -25,13 +29,18 @@ class TestErrorTemplateView:
     def test_as_error_view_includes_status_code(self):
 
         class TestErrorView(ErrorTemplateView):
+            """Dummy error view to test error view"""
             status_code = 113
             template_name = '400.html'
 
+        # Given a test view rendered as an error view
         view = TestErrorView.as_error_view()
 
-        request = RequestFactory(method="GET")
-        request.method = 'GET'
+        # and a get request
+        request = RequestFactory().get('/dummy')
+
+        # When calling the view on the request
         response = view(request)
 
+        # Then the response includes the status code from the error view
         assert response.status_code == 113
