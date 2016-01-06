f3063bb3379b4f1bbe4c4f6041caad2a454b0373
Adam Riggall <Adam.Riggall@nike.com>
2016-01-06 09:10:23 -0800

    Cleanup homepage tests

--- django/homepage/tests/test_views.py
+++ django/homepage/tests/test_views.py
@@ -1,17 +1,14 @@
 import unittest
-from unittest.mock import patch, sentinel
-
-from django.test import RequestFactory
+from unittest.mock import patch, sentinel, Mock
 
 from homepage.views import HomePageView
-from users.tests.factories import UserFactory
 
 
 class TestHomepageView(unittest.TestCase):
 
     def setUp(self):
-        self.user = UserFactory.stub()
-        self.request = RequestFactory()
+        self.user = Mock()
+        self.request = Mock()
         self.request.user = self.user
         view = HomePageView()
         view.request = self.request
@@ -22,19 +19,26 @@ class TestHomepageView(unittest.TestCase):
         self.view = view
 
     @patch('homepage.views.get_activities_for_user')
-    def test_get_queryset_calls_through_to_helper(self, mock_helper):
+    def test_get_queryset_calls_through_to_helper(self, mock_helper: Mock):
+        # Given a mock get_activities that returns a sentinel
         mock_helper.return_value = sentinel.activity_list
 
+        # When getting the queryset for the view
         queryset = self.view.get_queryset()
 
-        mock_helper.assert_called_once_with(self.user)
+        # Then the sentinel will be returned
         assert queryset == sentinel.activity_list
 
+        # and the mock will have been called with the current user
+        mock_helper.assert_called_once_with(self.user)
+
     @patch('homepage.views.get_leaders')
-    def test_get_context_data_populates_leaders(self, mock_helper):
+    def test_get_context_data_populates_leaders(self, mock_helper: Mock):
+        # Given a mock get_leaders that returns a sentinel
         mock_helper.return_value = sentinel.leaders
 
+        # When getting the context for the view
         context = self.view.get_context_data()
 
-        mock_helper.assert_called_once_with()
+        # Then the context will contain the sentinel
         assert context['leaders'] == sentinel.leaders
