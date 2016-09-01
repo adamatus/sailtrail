e7e1e31e122061944b12491acfb18fb7c1b4b4d4
Adam Riggall <adam@riggall.info>
2016-09-01 15:25:17 -0700

    chore: Update bump to push to Bitbucket

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function(grunt) {
                 tagName: 'v%VERSION%',
                 tagMessage: 'Version %VERSION%',
                 push: true,
-                pushTo: 'origin',
+                pushTo: 'bitbucket',
                 gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
                 globalReplace: false,
             },
